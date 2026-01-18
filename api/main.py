048e26b3e4271dc370353b16b22442147ab8ea94
illumi1717 <illumi.technologies@gmail.com>
2026-01-18 20:31:34 +0100

    add telegram bot logic

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api.routers.posts_router import router
+from api.routers.application_router import router as application_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,3 +14,4 @@ app.add_middleware(
 )
 
 app.include_router(router)
+app.include_router(application_router)
