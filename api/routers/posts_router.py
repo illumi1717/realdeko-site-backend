57e48cb2931e3e3c1b46c372e0de46c72bc34c1f
illumi1717 <illumi.technologies@gmail.com>
2026-01-18 15:41:37 +0100

    fix network_mode in api container

--- api/routers/posts_router.py
+++ api/routers/posts_router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
-from api.schemas.PostSchema import PostSchema
 from typing import List
 from dbase.collections.PostCollection import PostCollection
 
 router = APIRouter()
 
 @router.get("/all_posts")
-def get_all_posts() -> List[PostSchema]:
+def get_all_posts() -> List[dict]:
     collection = PostCollection()
     posts = collection.get_all_posts()
     return posts
